c8c0ecb65504c413f9b9fe56b60343f33432b068
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-03-15 01:54:37 +0700

    adjust footer position

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.scss';
-import { Login, Register, Forgot, Home, Error, Result, Profile, Product, Contact } from "./pages"
+import { Login, Register, Forgot, Home, Error, Result, Profile, Product, Contact, Topup } from "./pages"
 import { NavBar, Footer } from "./components"
 import {
   BrowserRouter as Router,
@@ -8,29 +8,36 @@ import {
   Route,
 } from "react-router-dom";
 import firebase from "firebase"
-import {firebaseConfig} from './firebase_config'
+import { firebaseConfig } from './firebase_config'
 
 
 const App = () => {
-  const userInfo = { username: "admin", amount: "999,999" }  
+  const userInfo = { username: "admin", amount: "999,999" }
   firebase.initializeApp(firebaseConfig)
 
   return (
-    <Router>
-      <NavBar status={1} userInfo={userInfo} />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/forgot" component={Forgot} />
-        <Route path="/result" component={Result} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/product" component={Product} />
-        <Route path="/contact" component={Contact} />
-        <Route path="/" component={Error} />
-      </Switch>
-      <Footer />
-    </Router>
+    <div className="page-container">
+      <Router>
+        <div className="content-wrap">
+          <NavBar status={1} userInfo={userInfo} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/forgot" component={Forgot} />
+            <Route path="/result" component={Result} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/product" component={Product} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/topup" component={Topup} />
+            <Route path="/" component={Error} />
+          </Switch>
+        </div>
+        <div className="footer">
+          <Footer />
+        </div>
+      </Router>
+    </div>
   )
 }
 
