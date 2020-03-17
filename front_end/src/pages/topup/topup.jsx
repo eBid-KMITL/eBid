effd033f5b0774c26896c301a050bc4a6c8af8c6
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-03-17 15:53:54 +0700

    new topup page

--- front_end/src/pages/topup/topup.jsx
+++ front_end/src/pages/topup/topup.jsx
@@ -1,10 +1,27 @@
 import React from "react"
+import topup from "../../assets/topup.png";
+import { Link } from "react-router-dom";
 
 export const Topup = () => {
   return (
     <div className="topup-main">
-      <div className="topup-container">
-        <h1>Topup Center : WIP</h1>
+      <div className="topup-wrap">
+        <div className="topup-banner">
+          <img src={topup} alt="eBid Logo" />
+        </div>
+        <div className="topup-container">
+          <h1>เติมเงิน</h1>
+          <form>
+            <div className="form-group">
+              <input type="text" name="Code" placeholder="กรอกรหัสเติมเงิน" required />
+            </div>
+            <div className="button-wrapper">
+              <button type="submit" className="btn">
+                ยืนยัน
+          </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )
