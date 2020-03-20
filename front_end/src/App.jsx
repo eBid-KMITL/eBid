a74be6a531126405a2e83b63700bbfcb28290d77
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-03-20 10:18:57 +0700

    remove footer for temporary

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.scss';
-import { Login, Register, Forgot, Home, Error, Result, Profile, Product, Contact, Topup } from "./pages"
+import { Login, Register, Forgot, Home, Error, Result, Profile, Product, Contact, Topup, Category } from "./pages"
 import { NavBar, Footer } from "./components"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import firebase from "firebase"
@@ -8,31 +8,27 @@ import { firebaseConfig } from './firebase_config'
 
 
 const App = () => {
-  const status = 1
-  const userInfo = { username: "admin", amount: "0" }
+  const status = 0
+  const userInfo = { username: "admin", amount: "9,999,999" }
   firebase.initializeApp(firebaseConfig)
 
   return (
     <div className="page-container">
       <Router>
-        <div className="content-wrap">
-          <NavBar status={status} userInfo={userInfo} />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-            <Route path="/forgot" component={Forgot} />
-            <Route path="/result" component={Result} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/product" component={Product} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/topup" component={Topup} />
-            <Route path="/" component={Error} />
-          </Switch>
-        </div>
-        <div className="footer">
-          <Footer />
-        </div>
+        <NavBar status={status} userInfo={userInfo} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/forgot" component={Forgot} />
+          <Route path="/result" component={Result} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/product" component={Product} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/topup" component={Topup} />
+          <Route path="/category" component={Category} />
+          <Route path="/" component={Error} />
+        </Switch>
       </Router>
     </div>
   )
