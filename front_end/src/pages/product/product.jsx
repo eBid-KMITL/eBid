fd0bf755f90d0f40c2188b69002d0c30bf7ccca2
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-04-03 17:51:24 +0700

    add query

--- front_end/src/pages/product/product.jsx
+++ front_end/src/pages/product/product.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 
 export const Product = () => {
+  const location = useLocation()
+  const { id } = queryString.parse(location.search)
+
   return (
     <div className="product-main">
       <div className="base-container">
-        PRODUCT_PAGE : WIP
+        PRODUCT_PAGE : {id}
       </div>
     </div>
   )
