8bb6ade38efae0ff6a4947c3dc5386539f659078
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-04-04 21:20:58 +0700

    fix not-login error in topup and profile

--- front_end/src/pages/profile/profile.jsx
+++ front_end/src/pages/profile/profile.jsx
@@ -3,31 +3,36 @@ import { Link } from "react-router-dom";
 import firebase from "firebase";
 import { Helmet } from "react-helmet";
 
-export const Profile = ({history}) => {
+export const Profile = ({ history }) => {
 
   function onLogout() {
     firebase.auth().signOut()
     history.replace('/')
+    window.location.reload(false);
   }
 
   return (
-    <div className="welcome-main">
-      <Helmet><title>Profile | eBid</title></Helmet>
-      <div className="head-text">
-        ยินดีต้อนรับ {firebase.auth().currentUser.displayName}
-      </div>
-      <div className="button-wrapper">
-        <Link to="/">
-          <button type="button" className="btn_s">
-            <u>กลับหน้าหลัก</u>
-          </button>
-        </Link>
-        <button type="button" className="btn"
-          onClick={onLogout}
-        >
-          ออกจากระบบ</button>
-      </div>
-    </div>
+    <>
+      {firebase.auth().currentUser ?
+        (<div className="welcome-main">
+          <Helmet><title>Profile | eBid</title></Helmet>
+          <div className="head-text">
+            ยินดีต้อนรับ {firebase.auth().currentUser.displayName}
+          </div>
+          <div className="button-wrapper">
+            <Link to="/">
+              <button type="button" className="btn_s">
+                <u>กลับหน้าหลัก</u>
+              </button>
+            </Link>
+            <button type="button" className="btn"
+              onClick={onLogout}
+            >
+              ออกจากระบบ</button>
+          </div>
+        </div>) : (window.location = "/login")
+      }
+    </>
   )
 
 }
