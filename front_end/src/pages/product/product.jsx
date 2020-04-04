fcce20606d7e320cc64b3264289a010a87136ca9
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-04-05 02:29:38 +0700

    add modal to product page

--- front_end/src/pages/product/product.jsx
+++ front_end/src/pages/product/product.jsx
@@ -1,17 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from 'react-router-dom';
+import Modal from 'react-responsive-modal';
+import './modal.scss';
+import { FaSyncAlt } from 'react-icons/fa';
 
 export const Product = () => {
   function useQuery() {
     return new URLSearchParams(useLocation().search);
   }
   const id = useQuery().get("id")
+  const [modal, setModal] = useState(false);
+  const [confirm, setConfirm] = useState(false);
+  function onOpenModal() {
+    setModal(true);
+  }
+  function onCloseModal() {
+    setModal(false);
+    setConfirm(false);
+  }
+  function onOpenConfirm() {
+    setConfirm(true);
+  }
+  function onCloseConfirm() {
+    setConfirm(false);
+  }
+  function check() {
+    window.alert("modal= " + modal + " confirm= " + confirm);
+  }
+  const formatter = new Intl.NumberFormat('th-TH', {
+    style: 'decimal',
+  });
+  var currentPrice = 9999;
+  var placeholder = "กรอกราคาที่มากกว่า " + formatter.format(currentPrice);
 
   return (
     <div className="product-main">
       <div className="base-container">
         PRODUCT_PAGE_ID : {id}
+        <button type="button" className="bid" onClick={() => onOpenModal()}>ประมูล</button>
+        <button type="button" className="check" onClick={() => check()}>CHECK</button>
+        <Modal open={modal} center={true} onClose={() => onCloseModal()}>
+          <h1>เสนอราคาประมูล</h1>
+          <div className="bid-form">
+            <form id="bid-price">
+              <input id="bid-price" type="number" placeholder={placeholder} step="10" min={currentPrice + 1} required />
+              <p id="ecoin-alert">เมื่อเสนอราคา eCoin ของท่านจะถูกกันไว้จนกว่าจะมีผู้เสนอราคาที่สูงกว่า</p>
+              <div className="form-foot">
+                <h3>
+                  ราคาปัจจุบัน : {formatter.format(currentPrice)} eCoin
+                  <button className="refresh" type="button" alt="Refresh" onClick={() => check()}><FaSyncAlt /></button>
+                </h3>
+                <button id="bid" type="submit" className="btn" alt="เสนอราคา" formTarget="hiddenFrame" onClick={() => onOpenConfirm()} >เสนอราคา</button>
+              </div>
+            </form>
+            <Modal open={confirm} center={true} showCloseIcon={false} closeOnEsc={false} closeOnOverlayClick={false} onClose={() => onCloseConfirm()} little>
+              <h1>ยืนยันเสนอราคา ?</h1>
+              <button id="bid" type="submit" className="btn" alt="เสนอราคา" formTarget="hiddenFrame" onClick={() => onCloseModal()}>ยืนยัน</button>
+              <button id="bid-confirm" type="submit" className="btn_s" alt="เสนอราคา" formTarget="hiddenFrame" onClick={() => onCloseConfirm()}>ยกเลิก</button>
+              <button id="bid-cancel" type="button" className="check" onClick={() => check()}>CHECK</button>
+            </Modal>
+          </div>
+        </Modal>
       </div>
+      <iframe name="hiddenFrame" width="0" height="0" border="0" style={{ display: "none" }}></iframe>
     </div>
   )
 }
