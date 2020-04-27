f723b7d7fb09255a8e4a5056e7d32c30ff0fd256
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-04-28 04:10:50 +0700

    edited a little change

--- front_end/src/pages/home/home.jsx
+++ front_end/src/pages/home/home.jsx
@@ -8,13 +8,13 @@ import p4 from "../../assets/products-pics/bag.jpg"
 
 export const Home = () => {
   const details = [
-    { name: "iPhone 11 64GB", price: 35800, owner: "e_shop", time:"2020-04-12T18:59+0700", nbid: 22, image: p1, id: 1 },
-    { name: "MacBook Pro 16\"", price: 47100, owner: "e_shop", time:"2020-04-15T09:59+0700", nbid: 15, image: p2, id: 2 },
-    { name: "VIGOROSO Watch", price: 7360, owner: "e_shop", time:"2020-04-16T11:59+0700", nbid: 9, image: p3, id: 3 },
-    { name: "กระเป๋าหนังแท้ญี่ปุ่น", price: 1328, owner: "e_shop", time:"2020-04-09T14:59+0700", nbid: 4, image: p4, id: 4 },
-    { name: "MacBook Pro 15\"", price: 47100, owner: "e_shop", time:"2020-04-22T22:59+0700", nbid: 21, image: p2, id: 5 },
+    { name: "iPhone 11 64GB", price: 35800, owner: "e_shop", time: "2020-04-12T18:59+0700", nbid: 22, img: p1, id: 1 },
+    { name: "MacBook Pro 16\"", price: 47100, owner: "e_shop", time: "2020-04-15T09:59+0700", nbid: 15, img: p2, id: 2 },
+    { name: "VIGOROSO Watch", price: 7360, owner: "e_shop", time: "2020-04-16T11:59+0700", nbid: 9, img: p3, id: 3 },
+    { name: "กระเป๋าหนังแท้ญี่ปุ่น", price: 1328, owner: "e_shop", time: "2020-04-09T14:59+0700", nbid: 4, img: p4, id: 4 },
+    { name: "MacBook Pro 15\"", price: 47100, owner: "e_shop", time: "2020-04-22T22:59+0700", nbid: 21, img: p2, id: 5 },
   ]
-  
+
   return (
     <div className="main">
       <Helmet><title>eBid - Online Bidding | Software Development Processes KMITL</title></Helmet>
