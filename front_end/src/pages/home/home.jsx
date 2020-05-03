eec2bfd7d782907cd406dacb789cc814a1025f63
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-05-03 19:07:24 +0700

    edited

--- front_end/src/pages/home/home.jsx
+++ front_end/src/pages/home/home.jsx
@@ -8,11 +8,11 @@ import p4 from "../../assets/products-pics/bag.jpg"
 
 export const Home = () => {
   const details = [
-    { name: "iPhone 11 64GB", price: 35800, owner: "e_shop", time: "2020-04-12T18:59+0700", nbid: 22, img: [p1], id: 1 },
-    { name: "MacBook Pro 16\"", price: 47100, owner: "e_shop", time: "2020-04-15T09:59+0700", nbid: 15, img: [p2], id: 2 },
-    { name: "VIGOROSO Watch", price: 7360, owner: "e_shop", time: "2020-04-16T11:59+0700", nbid: 9, img: [p3], id: 3 },
-    { name: "กระเป๋าหนังแท้ญี่ปุ่น", price: 1328, owner: "e_shop", time: "2020-04-09T14:59+0700", nbid: 4, img: [p4], id: 4 },
-    { name: "MacBook Pro 15\"", price: 47100, owner: "e_shop", time: "2020-04-22T22:59+0700", nbid: 21, img: [p2], id: 5 },
+    { name: "iPhone 11 64GB", price: 35800, owner: "eShop", time: "2020-05-12T18:59+0700", nbid: 22, img: [p1], id: 1 },
+    { name: "MacBook Pro 16\"", price: 31590, owner: "eShop", time: "2020-05-03T17:25+0700", nbid: 15, img: [p2], id: 2 },
+    { name: "VIGOROSO Watch", price: 7360, owner: "eShop", time: "2020-05-16T11:59+0700", nbid: 9, img: [p3], id: 3 },
+    { name: "กระเป๋าหนังแท้ญี่ปุ่น", price: 1328, owner: "eShop", time: "2020-05-09T14:59+0700", nbid: 4, img: [p4], id: 4 },
+    { name: "MacBook Pro 15\"", price: 47100, owner: "eShop", time: "2020-05-22T22:59+0700", nbid: 21, img: [p2], id: 5 },
   ]
 
   return (
