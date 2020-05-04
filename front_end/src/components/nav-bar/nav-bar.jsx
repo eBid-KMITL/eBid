b2753506464284f71b12f1a2e8fdd197909dfb1d
Thanapat Danpattanachaikul <thanapat255@gmail.com>
2020-05-05 00:03:16 +0700

    remove unsed component

--- front_end/src/components/nav-bar/nav-bar.jsx
+++ front_end/src/components/nav-bar/nav-bar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Moment from 'react-moment';
 import 'moment/locale/th';
 import logo from "../../assets/eBid.png";
