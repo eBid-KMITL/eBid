5f10ca8facb5c36a0bbdd1125aea36fae8234cc3
Pipek <zuzu.wc@gmail.com>
2020-05-06 16:31:58 +0700

    wow

--- front_end/src/components/user-account/user-account.jsx
+++ front_end/src/components/user-account/user-account.jsx
@@ -1,21 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import "./style.scss";
 import moneyPicture from "../../assets/eGold.png";
 import firebase from "firebase";
 import userinfo from "../../db/userinfo.json";
+import Modal from 'react-responsive-modal';
 
 export const UserAccount = ({ userData }) => {
   useEffect(() => {
     window.scrollTo(0, 0)
+
   }, [])
   const formatter = new Intl.NumberFormat('th-TH', {
     style: 'decimal',
   });
+  const [withdraw, setWithdraw] = useState(false);
+  const [money, setMoney] = useState(0)
+  function onOpenWithdraw() {
+    setWithdraw(true);
+  }
+  function onCloseWithdraw() {
+    setWithdraw(false);
+  }
 
-  function getMoney(){
-    let newBalance = userData.balance - 0 
+  function getMoney(e) {
+    e.preventDefault();
+    let withdrawMoney = parseInt(document.getElementById("bid-price").value) 
+    let newBalance = userData.balance - withdrawMoney
     firebase.firestore().collection('user').doc(userData.uid).update({
-      balance : newBalance
+      balance: newBalance
+    }).then(() => {
+      onCloseWithdraw()
     })
   }
 
@@ -36,11 +50,26 @@ export const UserAccount = ({ userData }) => {
         <nobr><b>ได้รับเงินแล้ว : </b>{formatter.format(userData.recieve)} eCoin</nobr>
         <br />
         <div className="button">
-          <button type="button" className="btn">
+          <button type="button" className="btn" onClick={() => onOpenWithdraw()}>
             ขอถอนเงิน
           </button>
         </div>
       </div>
+      <Modal open={withdraw} center={true} onClose={() => onCloseWithdraw()} little>
+        <h1>ถอนเงินจากบัญชี</h1>
+        <form name="bid-price" onSubmit={e => getMoney(e)}>
+          <input name="withdraw-input" id="bid-price" type="number" placeholder="กรอกจำนวนเงินที่ต้องการถอน" max={userData.balance} required />
+          <p id="ecoin-alert">กรอกจำนวนเงินที่ต้องการถอน</p>
+          <div className="form-foot">
+            <div className="price-container">
+              <p id="price-tag">เงินคงเหลือ</p>
+              <h3 id="curPrice">
+                {formatter.format(userData.balance)} eCoin</h3>
+              <button className="btn-close" id="close-terms" type="submit" >ถอนเงิน</button>
+            </div>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 };
--- front_end/src/components/user-mystatement-productdetail/productdetail.jsx
+++ front_end/src/components/user-mystatement-productdetail/productdetail.jsx
@@ -67,39 +67,41 @@ export const ProductDetail = ({ details, userData }) => {
       <td>
         <div className="productdetail-box">
           <div className="productdetail-img">
+            <Link title="ไปที่หน้าสินค้านี้" to={"/product?id=" + details.pid}>
             <img src={details?.img[0]} alt="statement-pic" className="prod-pic" />
+            </Link>
+        </div>
+        <div className="productdetail">
+          <div className="product-title"><Link title="ไปที่หน้าสินค้านี้" to={"/product?id=" + details.pid}>{details?.name}</Link></div>
+          <div className="product-owner">
+            <p style={{ margin: 0 }}>{"โดย " + details.owner}</p>
           </div>
-          <div className="productdetail">
-            <div className="product-title"><Link to={"/product?id=" + details.pid}>{details?.name}</Link></div>
-            <div className="product-owner">
-              <p style={{ margin: 0}}>{"โดย "+details.owner}</p>
-            </div>
-            <div className="time">
-              <Moment style={{ textDecoration: "underline" }} title={moment(due, "x").format("[หมดเวลาวันที่ ] DD MMMM YYYY [เวลา] HH:mm [น.]")} interval={1000} fromNow >{details.timeoutdate + "T" + details.timeoutclock + "+0700"}
-              </Moment>
-            </div>
+          <div className="time">
+            <Moment style={{ textDecoration: "underline" }} title={moment(due, "x").format("[หมดเวลาวันที่ ] DD MMMM YYYY [เวลา] HH:mm [น.]")} interval={1000} fromNow >{details.timeoutdate + "T" + details.timeoutclock + "+0700"}
+            </Moment>
           </div>
         </div>
-      </td>
-      <td>
-        <div className="status">{formatter.format(details.price)}</div>
-      </td>
-      <td>
-        <div className={details.currentWinner.uid === userData.uid ? 'winstatus' : 'losestatus'}>{
-          details.currentWinner.uid === userData.uid ? <p style={{ color: "rgb(51, 194, 51)" }}>ชนะ</p> : <p style={{ color: "red" }}>ไม่ชนะ</p>
-        }</div>
-      </td>
-      <td>
-        <div className="bid-status">
-          {
-            new Date(details.timeoutdate + "T" + details.timeoutclock + "+0700") > Date.now() ? "กำลังประมูล" : details.currentWinner.uid === userData.uid ?
-              (<><p>ชนะการประมูล</p>
-                <button className="btn" disabled={details.sent} onClick={() => confirmProduct(details.ownerid, details.price)}
-                > {details.sent ? "ยืนยันเรียบร้อยแล้ว" : "ยืนยันการรับสินค้า"}</button></>)
-              : ('ประมูลไม่สำเร็จ')
-          }
         </div>
       </td>
-    </tr>
+    <td>
+      <div className="status">{formatter.format(details.price)}</div>
+    </td>
+    <td>
+      <div className={details.currentWinner.uid === userData.uid ? 'winstatus' : 'losestatus'}>{
+        details.currentWinner.uid === userData.uid ? <p style={{ color: "rgb(51, 194, 51)" }}>ชนะ</p> : <p style={{ color: "red" }}>ไม่ชนะ</p>
+      }</div>
+    </td>
+    <td>
+      <div className="bid-status">
+        {
+          new Date(details.timeoutdate + "T" + details.timeoutclock + "+0700") > Date.now() ? "กำลังประมูล" : details.currentWinner.uid === userData.uid ?
+            (<><p>ชนะการประมูล</p>
+              <button className="btn" disabled={details.sent} onClick={() => confirmProduct(details.ownerid, details.price)}
+              > {details.sent ? "ยืนยันเรียบร้อยแล้ว" : "ยืนยันการรับสินค้า"}</button></>)
+            : ('ประมูลไม่สำเร็จ')
+        }
+      </div>
+    </td>
+    </tr >
   );
 };
